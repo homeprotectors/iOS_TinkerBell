48e284f4d58413b5dcc0ae284dd497e288ee524f
kacey <kacey0416@gmail.com>
2025-04-17 10:35:11 -0400

    🎉init

---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by Kacey Kim on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomeAppTests/HomeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppTests.swift
+//  HomeAppTests
+//
+//  Created by Kacey Kim on 4/17/25.
+//
+
+import Testing
+@testable import HomeApp
+
+struct HomeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
