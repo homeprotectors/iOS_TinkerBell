7aacb62a1500fd3930e20720f0778f37d157873d
kacey <kacey0416@gmail.com>
2025-04-21 19:41:30 -0400

    🎉 init

---
+++ DueMate/DueMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DueMateApp.swift
+//  DueMate
+//
+//  Created by Kacey Kim on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DueMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DueMateTests/DueMateTests.swift
@@ -0,0 +1,17 @@
+//
+//  DueMateTests.swift
+//  DueMateTests
+//
+//  Created by Kacey Kim on 4/21/25.
+//
+
+import Testing
+@testable import DueMate
+
+struct DueMateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
