19f830134a868f0a50edb991a3b30dcc58162a2a
kacey <kacey0416@gmail.com>
2025-05-05 15:58:01 -0400

    🎨style: define color assets

---
+++ DueMate/Extension/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color+Extension.swift
+//  DueMate
+//
+//  Created by Kacey Kim on 5/5/25.
+//
+
+import SwiftUI
+
+enum ListColor {
+    static let overdue = Color("Critical")
+    static let warning = Color("Warning")
+    static let normal = Color("Default")
+}
