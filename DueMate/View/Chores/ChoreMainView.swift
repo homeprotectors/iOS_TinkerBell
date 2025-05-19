a8f9e0213b80868f5aec0c7541a53fe5e3222a9c
kacey <kacey0416@gmail.com>
2025-05-19 18:37:55 -0400

    ✨feat: add reusable confirmation dialog component with dynamic content and callbacks

--- DueMate/View/Chores/ChoreMainView.swift
+++ DueMate/View/Chores/ChoreMainView.swift
@@ -9,59 +9,75 @@ import SwiftUI
 
 struct ChoreMainView: View {
     @StateObject private var viewModel = ChoreMainViewModel()
+    @State private var showDialog = false
+    @State private var selectedItem: ChoreItem? = nil
     
     var body: some View {
-        NavigationStack{
-            HStack{
-                Text("HOUSEHOLD\nLIST")
-                    .font(.system(size: 40, weight: .bold))
-                Spacer()
-                NavigationLink {
-                    ChoreCreateView(onComplete:{
-                        print("MainView: complete!!")
-                        viewModel.fetchChores()
-                    })
-                }label: {
-                    Image(systemName: "plus")
-                        .font(.system(size: 40))
-                        .foregroundStyle(.black)
+        ZStack {
+            NavigationStack{
+                HStack{
+                    Text("HOUSEHOLD\nLIST")
+                        .font(.system(size: 40, weight: .bold))
+                    Spacer()
+                    NavigationLink {
+                        ChoreCreateView(onComplete:{
+                            print("MainView: complete!!")
+                            viewModel.fetchChores()
+                        })
+                    }label: {
+                        Image(systemName: "plus")
+                            .font(.system(size: 40))
+                            .foregroundStyle(.black)
+                    }
+                    .padding()
+                }
+                .padding()
+                .padding(.top, 30)
+                
+                ScrollView {
+                    LazyVStack(spacing: 16) {
+                        ForEach(viewModel.items) { item in
+                            NavigationLink {
+                                ChoreDetailView(item: item)
+                                    .environmentObject(viewModel)
+                            }label: {
+                                ChoreItemView(item: item, color: viewModel.getListColor(due: item.nextDue), onCheckToggled: {
+                                    selectedItem = item
+                                    showDialog = true
+                                    
+                                } )
+                            }
+                            .buttonStyle(.plain)
+                            
+                        }
+                    }
+                    
+                    
+                    
                 }
                 .padding()
             }
-            .padding()
-            .padding(.top, 30)
+            .onAppear {
+                print("main onAppear")
+                viewModel.fetchChores()
+            }
+            .onChange(of: viewModel.shouldRefresh) {
+                if viewModel.shouldRefresh {
+                    print("main refresh")
+                    viewModel.fetchChores()
+                    viewModel.shouldRefresh = false
+                }
+            }
             
-            ScrollView {
-                LazyVStack(spacing: 16) {
-                    ForEach(viewModel.items) { item in
-                        NavigationLink {
-                            ChoreDetailView(item: item)
-                                .environmentObject(viewModel)
-                        }label: {
-                            ChoreItemView(item: item, color: viewModel.getListColor(due: item.nextDue), onCheckToggled: {
-                                //vm server networking
-                                print("check Toggeld!")
-                            } )
-                        }
-                        .buttonStyle(.plain)
+            if showDialog, let item = selectedItem {
+                ConfirmationDialog(
+                    isPresented: $showDialog,
+                    type: .mainViewCompletion,
+                    onConfirm: {
+                        viewModel.completeChore(item)
                         
                     }
-                }
-                
-                
-                
-            }
-            .padding()
-        }
-        .onAppear {
-            print("main onAppear")
-            viewModel.fetchChores()
-        }
-        .onChange(of: viewModel.shouldRefresh) {
-            if viewModel.shouldRefresh {
-                print("main refresh")
-                viewModel.fetchChores()
-                viewModel.shouldRefresh = false
+                )
             }
         }
         
