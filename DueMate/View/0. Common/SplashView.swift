4af9630cefeade5b64cf491361c2824f7ab67f77
kacey <kacey0416@gmail.com>
2025-12-22 17:26:40 -0500

    ✨feat: Add launch screen

---
+++ DueMate/View/0. Common/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  DueMate
+//
+//  Created by Kacey Kim on 12/18/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
---
+++ DueMate/ViewModel/5. Tutorial/TutorialViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TutorialViewModel.swift
+//  DueMate
+//
+//  Created by Kacey Kim on 12/3/25.
+//
+
+import Foundation
