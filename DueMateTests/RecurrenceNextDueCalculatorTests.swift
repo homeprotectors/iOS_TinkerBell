d30180fbd2fb5232f0310b8fc74bd00c95117821
kacey <kacey0416@gmail.com>
2026-07-28 22:02:59 -0400

    add more edge cases

--- DueMateTests/RecurrenceNextDueCalculatorTests.swift
+++ DueMateTests/RecurrenceNextDueCalculatorTests.swift
@@ -14,12 +14,20 @@ struct RecurrenceNextDueCalculatorTests {
     // date setting helper
     private var calendar: Calendar {
         var calendar = Calendar(identifier: .gregorian)
-        calendar.timeZone = TimeZone(secondsFromGMT: 0)!
+        calendar.timeZone = .current
         return calendar
     }
     
     private func makeDate(_ year: Int, _ month:Int, _ day: Int) -> Date {
         calendar.date(from: DateComponents(year: year, month: month, day: day))!
+    }
+    
+    @Test
+    func nextDueStrig_returnsString() {
+        let start = makeDate(2026,1,1)
+        let result = RecurrenceNextDueCalculator.nextDueString(for: .fixedDay([]), from: start, calendar: calendar)
+        
+        #expect(result == start.toString())
     }
     
     
@@ -41,10 +49,13 @@ struct RecurrenceNextDueCalculatorTests {
     
     @Test
     func perMonth_returnsOneMonthLater() {
-        let start = makeDate(2026,2,1)
-        let result = RecurrenceNextDueCalculator.nextDueDate(for: .perMonth, from: start, calendar:calendar)
+        let start1 = makeDate(2026,2,1)
+        let result1 = RecurrenceNextDueCalculator.nextDueDate(for: .perMonth, from: start1, calendar:calendar)
+        let start2 = makeDate(2026,1,31)
+        let result2 = RecurrenceNextDueCalculator.nextDueDate(for: .perMonth, from: start2, calendar:calendar)
         
-        #expect(result == makeDate(2026,3,1))
+        #expect(result1 == makeDate(2026,3,1))
+        #expect(result2 == makeDate(2026,2,28))
     }
     
     @Test
@@ -57,6 +68,42 @@ struct RecurrenceNextDueCalculatorTests {
             calendar: calendar
         )
         
-        #expect(result == makeDate(2026, 7, 27))
+        #expect(result == makeDate(2026, 7, 31))
     }
+    
+    @Test
+    func fixedDate_returnsCorrectly() {
+        let start1 = makeDate(2026, 7, 27)
+        let start2 = makeDate(2026, 2, 20)
+        
+        let result1 = RecurrenceNextDueCalculator.nextDueDate(
+            for: .fixedDate([.day(27), .day(1)]),
+            from: start1,
+            calendar: calendar
+        )
+        
+        let result2 = RecurrenceNextDueCalculator.nextDueDate(
+            for: .fixedDate([ .day(30)]),
+            from: start2,
+            calendar: calendar
+        )
+        
+        #expect(result1 == makeDate(2026, 8, 1))
+        #expect(result2 == makeDate(2026, 3, 30))
+    }
+    
+    @Test
+    func fixedMonth_returnsCorrectly() {
+        let start = makeDate(2026, 7, 27)
+        
+        let result = RecurrenceNextDueCalculator.nextDueDate(
+            for: .fixedMonth([.jan, .jul, .apr]),
+            from: start,
+            calendar: calendar
+        )
+        
+        #expect(result == makeDate(2027, 1, 27))
+    }
+    
+    
 }
